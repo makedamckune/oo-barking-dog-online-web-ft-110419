1edef69b201ae03ccbd728f156775d5d299039c1
Makeda P Mckune <makedamckune@gmail.com>
2019-11-13 02:31:26 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Dog 
+  def name= (dog_name)
+    @give_dog_name = dog_name 
+    end 
+  def name
+   @give_dog_name
+    
+  end 
+   
+    def bark 
+      puts "woof!"
+    end 
+end 
+
+fido = Dog.new
+fido.name = "Fido"
+
+
+fido.bark
+
